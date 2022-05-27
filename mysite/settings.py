3ffaecae80ea30289a7b17651ac6a4019440cc4b
Kashvi <malikkaashvi@gmail.com>
2022-05-28 00:45:30 +0530

    remove sc

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9bba#e*60s)vr+7grq^ovy^=a#(94ai)ri$_z1x-ivzkv5j3v('
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
